72eba12d48376533ad79fc79e460c7fc4826baf6
gdagm94 <65061362+gdagm94@users.noreply.github.com>
2024-05-16 16:49:44 -0400

    Update puppeteer_script.js

--- js/puppeteer_script.js
+++ js/puppeteer_script.js
@@ -1,24 +1,23 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Navigate to the HTML page containing the chat box interface
-  await page.goto('file://' + __dirname + '/index.html');
+  // Construct the file path to your HTML file
+  const filePath = 'file://' + path.resolve(__dirname, '../index.html');
 
-  // Wait for the chat box interface to be loaded
-  await page.waitForSelector('#chat-box');
+  // Navigate to the HTML page containing the chat box interface
+  await page.goto(filePath);
+
+
+// Wait for the response from the chat box interface
+  await page.waitForSelector('.chat-message');
 
   // Interact with the chat box interface (example: sending a message)
   await page.type('#chat-input', 'Hello, this is a test message!');
   await page.click('#send-message');
-
-  // Wait for the response from the chat box interface
-  await page.waitForSelector('.chat-message');
-
-  // Capture a screenshot of the page (optional)
-  await page.screenshot({ path: 'screenshot.png' });
 
   // Close the browser
   await browser.close();
