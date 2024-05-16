03502cbaf28a29c7fcd3fd2cf25cf271c9014d8d
gdagm94 <65061362+gdagm94@users.noreply.github.com>
2024-05-16 13:28:14 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -38,3 +38,10 @@ function make_me_dark() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
