b160d3882177c9cc6dc78d64e50cfa7e70c8a3b0
gdagm94 <65061362+gdagm94@users.noreply.github.com>
2024-05-17 12:22:29 -0400

    Create cf_worker.js

---
+++ js/cf_worker.js
@@ -0,0 +1,22 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  // Only allow specific origins to access this endpoint for security
+  const allowedOrigins = ['https://dagmawieshete.com'];
+  const origin = request.headers.get('Origin');
+  if (!allowedOrigins.includes(origin)) {
+    return new Response('Forbidden', { status: 403 });
+  }
+
+  // Fetch the API key from the environment variable
+  const apiKey = ResumeCF;
+  return new Response(JSON.stringify({ apiKey }), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Methods': 'GET'
+    },
+  });
+}
